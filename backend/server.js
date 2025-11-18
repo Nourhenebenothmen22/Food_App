aa5f074c124398c0c9fe1376f74b13ca3e4174e1
nourhene <benothmennourhen8@gmail.com>
2025-11-18 20:41:27 +0100

    🔧 server.js: Amélioration configuration CORS et sécurité

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(helmet({
 
 // Configuration CORS complète
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5000'],
+  origin: ['http://localhost:5173','http://localhost:5174', 'http://localhost:5000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
