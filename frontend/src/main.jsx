98cba545230a8d6e8ca6231eb0541b73062ad2b9
nourhene <benothmennourhen8@gmail.com>
2025-11-18 20:41:58 +0100

    ⚙️ main.jsx: Configuration ToastContainer pour notifications globales

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import StoreContextProvider from './context/StoreContext.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import StoreContextProvider from './context/StoreContext.jsx';
+
+// Import toast
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <StoreContextProvider>
-          <App />
-    </StoreContextProvider>
-  
+      <StoreContextProvider>
+        <App />
+        {/* Container des toasts */}
+        <ToastContainer 
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+        />
+      </StoreContextProvider>
     </BrowserRouter>
   </StrictMode>
-)
+);
