ef14343eb71cdee5db6ddddc3aa812f6da807734
nourhene <benothmennourhen8@gmail.com>
2025-11-22 21:29:32 +0100

    ⚛️ app: update routing and component structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,54 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Cart from './pages/Cart/Cart';
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import Layout from './pages/Layout';
-import Profile from './pages/Profile/Profile';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Layout from "./pages/Layout";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Routes>
         {/* Routes avec Layout (navbar + footer) */}
-        <Route path='/' element={
-          <Layout>
-            <Home />
-          </Layout>
-        } />
-        <Route path='/cart' element={
-          <Layout>
-            <Cart />
-          </Layout>
-        } />
-        <Route path='/order' element={
-          <Layout>
-            <PlaceOrder />
-          </Layout>
-        } />
-        
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <Layout>
+              <Cart />
+            </Layout>
+          }
+        />
+        <Route
+          path="/order"
+          element={
+            <Layout>
+              <PlaceOrder />
+            </Layout>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <Layout>
+              <Profile />
+            </Layout>
+          }
+        />
+
         {/* Routes sans Layout (pas de navbar/footer) */}
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/profile' element={<Profile/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
