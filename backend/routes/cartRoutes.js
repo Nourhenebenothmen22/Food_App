18fc27af8d12a5f6d245901945f5fc37cbb76d37
nourhene <benothmennourhen8@gmail.com>
2025-11-26 21:42:33 +0100

    🔗 routes/cartRoutes.js: update cart API endpoints

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { addToCart,removeFromCart } = require('../controllers/cartController');
+const { addToCart,removeFromCart,getCart} = require('../controllers/cartController');
 const router = express.Router();
 
 // Ajouter / Retirer via POST
 router.post('/add', addToCart);
 router.post('/remove',removeFromCart )
+router.post('/get',getCart)
 
 
 module.exports = router;
