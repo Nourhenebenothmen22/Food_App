cc12ad58bf7cc3b1d76b21bd2316a7cde045f75f
nourhene <benothmennourhen8@gmail.com>
2025-11-27 21:54:02 +0100

    Add new order endpoint to handle order creation and retrieval

--- backend/server.js
+++ backend/server.js
@@ -86,6 +86,8 @@ app.get('/', (req, res) => {
 app.use('/api/v1/auth', require('./routes/authRoutes'));
 app.use('/api/v1/food', require('./routes/FoodRoutes'));
 app.use('/api/v1/cart', require('./routes/cartRoutes'));
+app.use('/api/v1/order', require('./routes/OrderRoutes'));
+
 
 // ------------------------- ERROR HANDLING -------------------------
 app.use((req, res) => {
