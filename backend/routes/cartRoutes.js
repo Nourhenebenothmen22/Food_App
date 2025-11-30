9db489614d1e0e4dd3b581f41d1722005d63da2d
nourhene <benothmennourhen8@gmail.com>
2025-11-30 15:30:45 +0100

    refactor(routes): update cart get endpoint to use GET with userId parameter

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { addToCart,removeFromCart,getCart} = require('../controllers/cartController');
+const { addToCart, removeFromCart, getCart } = require('../controllers/cartController');
 const router = express.Router();
 
 // Ajouter / Retirer via POST
 router.post('/add', addToCart);
-router.post('/remove',removeFromCart )
-router.post('/get',getCart)
-
+router.post('/remove', removeFromCart);
+// Changer POST en GET avec paramètre
+router.get('/get/:userId', getCart);
 
 module.exports = router;
