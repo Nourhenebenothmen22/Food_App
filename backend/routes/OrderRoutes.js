81b0a610d7ec51974e65c8cabb8994a28072e8db
nourhene <benothmennourhen8@gmail.com>
2025-11-30 16:37:00 +0100

    feat(routes): update order endpoints to handle both cash and card payment methods

--- backend/routes/OrderRoutes.js
+++ backend/routes/OrderRoutes.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const router=express.Router()
-const {placeOrder}=require('../controllers/OrderController')
+const {placeOrder, getOrderById}=require('../controllers/OrderController')
 const authMiddleware=require('../middlewares/authMiddleware')
 router.post('/place',authMiddleware,placeOrder)
+router.get('/:orderId', authMiddleware, getOrderById);
 module.exports=router
